e30a834e1dcd58b7b67f7ed0da1d5a4aef8db9b6
roger <rogeranyor@gmail.com>
2021-04-17 23:44:00 +0200

    Grid View para notas ViewPage para deslizar Navigation View casi hecha

--- app/src/main/java/com/example/postover/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/postover/ui/home/HomeFragment.java
@@ -1,35 +1,52 @@
 package com.example.postover.ui.home;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.example.postover.MainActivity;
 import com.example.postover.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+
+        String [] number = {"1", "2","3"};
+        int [] numberimage= {R.drawable.one,R.drawable.two,R.drawable.three};
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        GridView gridView = root.findViewById(R.id.grid_view) ;
+        MainAdapter mainAdapter = new MainAdapter(getActivity(),inflater,number,numberimage);
+        gridView.setAdapter(mainAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(),"You clicked "+number[+position],Toast.LENGTH_SHORT).show();
             }
         });
+
+
         return root;
     }
 }
