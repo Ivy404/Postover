10a9eb3e607b05666ab0e8a56edff29f5f2921c8
Marcos <mpalomvi8@alumnes.ub.edu>
2021-04-18 22:01:15 +0200

    firebase funcionando perfee brrr

--- app/src/main/java/com/example/postover/MainActivity.java
+++ app/src/main/java/com/example/postover/MainActivity.java
@@ -6,16 +6,26 @@ import android.view.Menu;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.postover.Model.Client;
 import com.example.postover.onboarding.AdapterSlide;
 import com.example.postover.ui.gallery.GalleryFragment;
 import com.example.postover.ui.home.HomeFragment;
 import com.example.postover.ui.slideshow.SlideshowFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
@@ -31,13 +41,21 @@ import androidx.viewpager2.widget.ViewPager2;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity   {
+public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private AlertDialog.Builder dialogBuilder;
     private AlertDialog dialog;
-    private EditText username,password,mail,name;
-    private Button register;
+    private EditText editTextUsername, editTextPassword, editTextEmail, editTextName;
+
+    private EditText loginMail, loginPassword;
+    private String email, name, username, password;
+    private String mailLogin, passwordLogin;
+
+    //cositas del firebase
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +65,8 @@ public class MainActivity extends AppCompatActivity   {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         FloatingActionButton fab = findViewById(R.id.fab);
 
@@ -75,7 +95,7 @@ public class MainActivity extends AppCompatActivity   {
         fragmentList.add(new GalleryFragment());
         fragmentList.add(new HomeFragment());
         fragmentList.add(new SlideshowFragment());
-        AdapterSlide adapter = new AdapterSlide(getSupportFragmentManager(),getLifecycle(),fragmentList);
+        AdapterSlide adapter = new AdapterSlide(getSupportFragmentManager(), getLifecycle(), fragmentList);
         ViewPager2 viewPager2 = findViewById(R.id.view_pager2);
         viewPager2.setAdapter(adapter);
         viewPager2.setCurrentItem(1);
@@ -114,25 +134,37 @@ public class MainActivity extends AppCompatActivity   {
                 || super.onSupportNavigateUp();
     }
 
-    public void createLoginDialog(View v){
+    public void createLoginDialog(View v) {
         dialogBuilder = new AlertDialog.Builder(this);
         final View loginPopupView = getLayoutInflater().inflate(R.layout.popup_login, null);
+
+        Button login = (Button) loginPopupView.findViewById(R.id.btn_login);
+        loginMail = (EditText) loginPopupView.findViewById(R.id.pt_username);
+        loginPassword = (EditText) loginPopupView.findViewById(R.id.pt_password);
 
         dialogBuilder.setView(loginPopupView);
         dialog = dialogBuilder.create();
         dialog.show();
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mailLogin = loginMail.getText().toString();
+                passwordLogin = loginPassword.getText().toString();
+                loginUser();
+            }
+        });
+
     }
 
-    public void createRegisterDialog(View v){
-        dialogBuilder = new AlertDialog.Builder(this);
-        final View registerPopupView = getLayoutInflater().inflate(R.layout.popup_register, null);
-        username = (EditText) registerPopupView.findViewById(R.id.pt_usernameRegister);
-        name = (EditText) registerPopupView.findViewById(R.id.pt_fullnameRegister);
-        password = (EditText) registerPopupView.findViewById(R.id.pt_TextPassword);
-        mail = (EditText) registerPopupView.findViewById(R.id.pt_mailRegister);
+    public void createRegisterDialog(View v) {
 
-        register = (Button) registerPopupView.findViewById(R.id.btn_registerRegister);
+        final View registerPopupView = getLayoutInflater().inflate(R.layout.popup_register, null);
+        editTextUsername = (EditText) registerPopupView.findViewById(R.id.pt_usernameRegister);
+        editTextName = (EditText) registerPopupView.findViewById(R.id.pt_fullnameRegister);
+        editTextPassword = (EditText) registerPopupView.findViewById(R.id.pt_TextPassword);
+        editTextEmail = (EditText) registerPopupView.findViewById(R.id.pt_mailRegister);
+        Button register = (Button) registerPopupView.findViewById(R.id.btn_registerRegister);
 
         dialogBuilder.setView(registerPopupView);
         dialog = dialogBuilder.create();
@@ -141,13 +173,62 @@ public class MainActivity extends AppCompatActivity   {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                registerNewUser();
+                name = editTextName.getText().toString();
+                username = editTextUsername.getText().toString();
+                password = editTextPassword.getText().toString();
+                email = editTextEmail.getText().toString();
+
+                if (!name.isEmpty() && !email.isEmpty() && !username.isEmpty() && !password.isEmpty()) {
+                    if (password.length() >= 6) {
+                        registerNewUser();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Password isn't strong enough", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Complete all fields", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
-    private void registerNewUser(){
-        Client cliente = new Client(name.getText().toString(),password.getText().toString(),mail.getText().toString(),username.getText().toString());
+    private void registerNewUser() {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Client cliente = new Client(name, password, email, username);
+                    String id = mAuth.getCurrentUser().getUid();
+                    mDatabase.child("users").child(id).setValue(cliente).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            if (task2.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "User added", Toast.LENGTH_LONG).show();
+                                dialog.dismiss();
+                            } else {
+                                Toast.makeText(MainActivity.this, "User could not be created", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
+
+    public void loginUser() {
+        mAuth.signInWithEmailAndPassword(mailLogin, passwordLogin).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Toast.makeText(MainActivity.this, "Success! login completed", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                } else {
+                    Toast.makeText(MainActivity.this, "Error! These credentials do not match our records", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 }
