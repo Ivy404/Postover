5f1b17296fd71bfad5bb1eef7859327f65d6caa4
Couci27 <couceiromato.alejandro@gmail.com>
2021-05-01 20:11:03 +0200

    home button

--- app/src/main/java/com/example/postover/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/postover/ui/home/HomeFragment.java
@@ -1,15 +1,19 @@
 package com.example.postover.ui.home;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,33 +24,38 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.example.postover.MainActivity;
+import com.example.postover.Model.HomeNote;
+import com.example.postover.Model.Note;
 import com.example.postover.R;
+import com.example.postover.ui.ActivityRegister;
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 public class HomeFragment extends Fragment {
-
-
-
+    ArrayList<String> notes = new ArrayList<String>();
+    int cont;
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-
-
-        String [] number = {"1", "2","3"};
-        int [] numberimage= {R.drawable.one,R.drawable.two,R.drawable.three};
+        String[] number = {"1", "2", "3"};
+        int[] numberimage = {R.drawable.one, R.drawable.two, R.drawable.three};
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-
-        GridView gridView = root.findViewById(R.id.grid_view) ;
-        MainAdapter mainAdapter = new MainAdapter(getActivity(),inflater,number,numberimage);
+        root.findViewById(R.id.home_fab).setOnClickListener(this::AddNote);
+        GridView gridView = root.findViewById(R.id.grid_view);
+        MainAdapter mainAdapter = new MainAdapter(getActivity(), inflater, number, numberimage);
         gridView.setAdapter(mainAdapter);
-
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getContext(),"You clicked "+number[+position],Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "You clicked " + number[+position], Toast.LENGTH_SHORT).show();
             }
         });
 
 
         return root;
+    }
+
+    public void AddNote(View view){
+        cont++;
+        Toast.makeText(getActivity(),String.valueOf(cont), Toast.LENGTH_SHORT).show();
     }
 }
