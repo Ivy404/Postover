3910f2c7183d88360eeafc19029dfe8d81c34a44
Couci27 <couceiromato.alejandro@gmail.com>
2021-05-02 21:31:42 +0200

    HomeNote cards

--- app/src/main/java/com/example/postover/ui/home/MainAdapter.java
+++ app/src/main/java/com/example/postover/ui/home/MainAdapter.java
@@ -8,13 +8,19 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.postover.Model.HomeNote;
 import com.example.postover.Model.ToDoNote;
 import com.example.postover.R;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Date;
 import java.util.List;
 
-public class MainAdapter extends BaseAdapter {
+/*public class MainAdapter extends BaseAdapter {
 
     private final Context mContext;
     private List<HomeNote> notes;
@@ -56,4 +62,53 @@ public class MainAdapter extends BaseAdapter {
         notifyDataSetChanged();
     }
 
+}*/
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+    private List<HomeNote> notes;
+    private LayoutInflater mInflater;
+    private Context context;
+
+    public MainAdapter(List<HomeNote> homeNotes, Context context){
+        this.mInflater = LayoutInflater.from(context);
+        this.notes = homeNotes;
+        this.context = context;
+    }
+
+    public int getItemCount(){
+        return notes.size();
+    }
+
+    @NonNull
+    @Override
+    public MainAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.fragment_homenote_task, null);
+        return new MainAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MainAdapter.ViewHolder holder, int position) {
+        holder.bindData(notes.get(position));
+    }
+
+    public void setNotes(List<HomeNote> notes){
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        TextView text;
+        ViewHolder(View itemView){
+            super(itemView);
+            title = itemView.findViewById(R.id.cardTitle);
+            text = itemView.findViewById(R.id.cardText);
+        }
+        void bindData(@NotNull HomeNote note){
+            title.setText(note.getTitle());
+            text.setText(note.getText());
+        }
+
+
+    }
 }
