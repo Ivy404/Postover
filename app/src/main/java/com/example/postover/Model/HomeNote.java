a553f835682966c18864c3c6fd00dc838484e655
roger <rogeranyor@gmail.com>
2021-05-28 17:24:36 +0200

    Ahora las notas pueden contener varias imagenes y escribir debajo y encima de las imagenes

--- app/src/main/java/com/example/postover/Model/HomeNote.java
+++ app/src/main/java/com/example/postover/Model/HomeNote.java
@@ -2,14 +2,19 @@ package com.example.postover.Model;
 
 import android.media.Image;
 import android.os.Parcel;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.postover.Splash;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class HomeNote extends Note{
     private String text;
-    private ArrayList<Image> images;
+    private List<String> images,texts;
     //Falta Content
 
     public HomeNote(String title){
@@ -18,19 +23,29 @@ public class HomeNote extends Note{
         this.setTitle(title);
         this.setText("");
         images = new ArrayList<>();
+        texts = new ArrayList<>();
     }
     public HomeNote(){}
+
+    public List<String> getTexts() {
+        return texts;
+    }
+
+    public void setTexts(List<String> texts) {
+        this.texts = texts;
+    }
+
     @Override
     public String getTitle() {
         return this.title;
     }
 
-    public ArrayList<Image> getImages() {
+    public List<String> getImages() {
         return images;
     }
 
-    public void setImages(ArrayList<Image> images) {
-        this.images = images;
+    public void setImages(ArrayList<String> ImageView) {
+        this.images = ImageView;
     }
 
     @Override
