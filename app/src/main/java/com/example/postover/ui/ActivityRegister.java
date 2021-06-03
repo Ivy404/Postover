d02cd3444c82ee860c9eef6c40815e3c9dd65891
roger <rogeranyor@gmail.com>
2021-06-04 00:02:42 +0200

    cosas sin importancia

--- app/src/main/java/com/example/postover/ui/ActivityRegister.java
+++ app/src/main/java/com/example/postover/ui/ActivityRegister.java
@@ -25,6 +25,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.OAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
